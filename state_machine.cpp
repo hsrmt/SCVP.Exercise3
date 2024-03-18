3f273e45565866f15d50d437e008d64bc535c7bd
hatice seremet <haticeesrmt52@gmail.com>
2024-03-18 11:16:09 -0400

    stateMachine task1

--- state_machine.cpp
+++ state_machine.cpp
@@ -2,4 +2,37 @@
 
 #include "state_machine.h"
 
-// TODO
+
+// FSM logic implementation
+void stateMachine::process() {
+    switch (currentState) {
+        case Start:
+            if (input.read() == 'G') currentState = G;
+            else if(input.read() == 'A' || input.read() == 'C' || input.read() == 'T') currentState==Start;
+            break;
+        case G:
+            if (input.read() == 'A') currentState = GA;
+            else if (input.read() == 'G') currentState = G;
+            else if(input.read() == 'T' || input.read() == 'C') currentState = Start;
+            break;
+        case GA:
+            if (input.read() == 'A') currentState = GAA;
+            else if (input.read() == 'G') currentState = G;
+            else if(input.read() == 'T' || input.read() == 'C') currentState = Start;
+            break;
+        case GAA:
+            if (input.read() == 'G') {
+                currentState = GAAG;
+                std::cout << "GAAG sequence detected!" << std::endl;
+            }
+            else if( input.read() == 'A' || input.read() == 'T' || input.read() == 'C') currentState = Start;
+            break;
+        case GAAG:
+            if (input.read() == 'G') currentState = G;
+            else if(input.read() == 'A' || input.read() == 'C' || input.read() == 'T') currentState==Start;
+            break;
+        default:
+            currentState = Start; // Fallback to Start state in case of an undefined state
+            break;
+    }
+}
--- state_machine.h
+++ state_machine.h
@@ -3,8 +3,26 @@
 
 #include <systemc.h>
 
-// TODO
+SC_MODULE(stateMachine) {
+public:
+    sc_in<char> input; // DNA base input
+    sc_in<bool> clk;  // Clock input
+
+    // Constructor
+    SC_CTOR(stateMachine){
+        SC_METHOD(process);
+        sensitive << clk.pos();
+        dont_initialize();
+    }
+
+    // State enumeration
+    enum base {Start, G, GA, GAA, GAAG};
+
+private:
+    void process(); // FSM logic
+
+    // Current state variable
+    base currentState;
+};
 
 #endif // STATE_MACHINE_H
-
-
